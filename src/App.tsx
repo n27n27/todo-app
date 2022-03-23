42dbd59c0c24c389ddf00d2210aa8159f1bd2dd3
n27 <skfdkfkalsrn@gmail.com>
2022-03-23 09:52:56 +0900

    Todoinsert 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
 
 const App = () => {
 
-  return <TodoTemplate>Todo App을 만들자 !</TodoTemplate>
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+
+  );
 
 };
 
